3d02d5003093add3abb23b8b5d19c459b20f3d68
tinhmenhdo <tinhmenhdo@gmail.com>
2024-11-28 21:05:19 +0700

    Updates

--- VJHgSw4z/B_3wc3ML.js
+++
@@ -1,1 +0,0 @@
-const yinyang2=new Proxy({src:"/VJHgSw4z/DS3CXu3F.webp",width:1024,height:1024,format:"webp"},{get:(s,e,t)=>"clone"===e?structuredClone(s):"fsPath"===e?"/root/code/tmd_astro/src/assets/images/yinyang2.webp":(void 0!==s[e]&&globalThis.astroAsset&&globalThis.astroAsset?.referencedImages.add("/root/code/tmd_astro/src/assets/images/yinyang2.webp"),s[e])});export{yinyang2 as default};
