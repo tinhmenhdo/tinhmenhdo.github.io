9c720c31726c8c109feb20492b231f59ebb9f881
tinhmenhdo <tinhmenhdo@gmail.com>
2025-03-14 19:41:33 +0700

    Updates

--- 3Xj2yoi9/Ds0YSbeu.js
+++
@@ -1,1 +0,0 @@
-const _default=new Proxy({src:"/3Xj2yoi9/DmKpoRSo.png",width:800,height:1018,format:"png"},{get:(s,t,e)=>"clone"===t?structuredClone(s):"fsPath"===t?"/root/code/tmd_astro/src/assets/images/default.png":(void 0!==s[t]&&globalThis.astroAsset&&globalThis.astroAsset?.referencedImages.add("/root/code/tmd_astro/src/assets/images/default.png"),s[t])});export{_default as default};
